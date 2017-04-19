cdf6d451f5220590d3e4633863d39587612238f1
Marlon Lucio <marlonlucioss@gmail.com>
2017-04-19 13:06:02 -0300

    Alguns ajustes na base

--- wp-content/themes/hueman/sidebar.php
+++ wp-content/themes/hueman/sidebar.php
@@ -7,28 +7,6 @@
 
 		<div class="sidebar-content">
 
-			<?php if ( hu_is_checked('sidebar-top') ): ?>
-  			<div class="sidebar-top group">
-          <?php if ( hu_has_social_links() ) : ?>
-              <p><?php _e('Follow:','hueman'); ?></p>
-          <?php else : //if not customizing, display an empty p for design purposes ?>
-              <?php if ( ! hu_is_customizing() && is_user_logged_in() && current_user_can( 'edit_theme_options' ) && is_super_admin() ) : ?>
-                  <?php
-                    printf( '<p style="text-transform:none;font-size: 0.8em;">%1$s. <a style="color: white;text-decoration:underline;" href="%2$s" title="%3$s">%3$s &raquo;</a></p>',
-                        __('You can set your social links here from the live customizer', 'hueman'),
-                        admin_url( 'customize.php?autofocus[section]=social_links_sec' ),
-                        __('Customize now', 'hueman')
-                    );
-                  ?>
-              <?php elseif ( ! is_user_logged_in() ) : ?>
-                  <?php printf('<p>&nbsp;</p>'); ?>
-              <?php endif; ?>
-          <?php endif; ?>
-
-          <?php hu_print_social_links() ; ?>
-  			</div>
-			<?php endif; ?>
-
 			<?php if ( hu_get_option( 'post-nav' ) == 's1') { get_template_part('parts/post-nav'); } ?>
 
 			<?php if( is_page_template('page-templates/child-menu.php') ): ?>
